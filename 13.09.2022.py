04ec71852f8eff4f4bba9a200ff8874379c8c6c9
malinovskiss <90186408+malinovskiss@users.noreply.github.com>
2022-09-13 12:57:45 +0300

    Add files via upload

---
+++ 13.09.2022.py
@@ -0,0 +1,22 @@
+class Rekins():
+    def _init_(self,klients,veltijums,izmers,materials):
+        self.klients = klients
+        self.veltijums = veltijums
+        self.izmers = izmers
+        self.materials = materials
+
+    def izdrukat(self):
+        print("klients:",self.klients)
+        print("veltijums:",self.veltijums)
+        print("izmers:",self.izmers)
+        print("materials:",self.materials)
+
+    def aprekins():
+        darba_samaksa = 15
+        PVN = 22
+        produkta_cena = (veltijums teksta garums simbolos) * 1.2 + (platums/100 * garums/100 * augstums/100)/ 3 * materials
+        PVN_summa = (produkta_cena + darba_samaksa)*PVN/100
+        rekina_summa = (produkta_cena + darba_samaksa) + PVN_summa
+
+    
+pirmais = Rekins("Anna","Lai izdodas!",[20,20,20],3,5)
