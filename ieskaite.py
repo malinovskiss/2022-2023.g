42f4ed0acc3ede259d02fe2b4a25c1f7a6ea8809
malinovskiss <90186408+malinovskiss@users.noreply.github.com>
2022-10-17 11:11:06 +0300

    Update ieskaite.py

--- ieskaite.py
+++ ieskaite.py
@@ -2,12 +2,30 @@
 https://replit.com/@malinovskiss/CloseUnsightlyParticle#main.py
 
 2.uzd
-
+https://replit.com/@malinovskiss/CloseUnsightlyParticle#main(2.uzd).py
 
 3.uzd
-
+https://replit.com/@malinovskiss/CloseUnsightlyParticle#main(3.uzd).py
 
 4.uzd
-
+https://replit.com/@malinovskiss/CloseUnsightlyParticle#main(4.uzd).py
 
 5.uzd
+import bibliotekas
+
+print(bibliotekas.a)
+
+for prece in bibliotekas.saraksts:
+    print(prece)
+
+import bibliotekas as bibl
+
+print(bibl.a)
+
+for prece in bibl.saraksts:
+    print(prece)
+
+print(bibl.summa(5,5))
+
+bmw = bibl.Auto()
+bmw.braukt()
