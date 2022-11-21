07dec0b0ede087ac3ec44ddabc3b149b4e6d6deb
malinovskiss <90186408+malinovskiss@users.noreply.github.com>
2022-11-21 11:51:50 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const API = "http://127.0.0.1:81/"
+let manaZina = document.querySelector('manaZina')
+let chataZinas = document.querySelector(',chatazinas')
+let vards = document.querySelector('.vards')
+
+function sutitZinu(){
+    console.log("sutitzinu() darbojas")
+    chataZinas.innerHTML +u <br> + manaZina.value 
+    fetch(API+'/sutit/'+vards.value+'/'+manaZina.value)
