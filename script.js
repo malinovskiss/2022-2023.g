746e506691e2182ea457cbe75b64a53dc7dbcca0
malinovskiss <90186408+malinovskiss@users.noreply.github.com>
2022-11-22 12:23:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const API = "http://127.0.0.1:81/"
-let manaZina = document.querySelector('manaZina')
-let chataZinas = document.querySelector(',chatazinas')
+const API = 'http://127.0.0.1:81'
+
+let myMsg = document.querySelector('.myMsg')
+let chatMsg = document.querySelector('.chatazinas')
 let vards = document.querySelector('.vards')
 
 function sutitZinu(){
-    console.log("sutitzinu() darbojas")
-    chataZinas.innerHTML +u <br> + manaZina.value 
-    fetch(API+'/sutit/'+vards.value+'/'+manaZina.value)
+    console.log('sutitzinu() darbojas')
+    chatMsg.innerHTML += '<br/>' + myMsg.value
+    fetch(API+'/send/'+vards.value+'/'+myMsg.value)
 }
