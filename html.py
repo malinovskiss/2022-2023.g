fa8776c72367c7b80de57a888e80465cf56bde9f
malinovskiss <90186408+malinovskiss@users.noreply.github.com>
2022-11-28 10:45:55 +0200

    Update and rename html to html.py

---
+++ html.py
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>WeChatTOGETHER!</title>
+        <link href="style.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+    <div class="container">
+        <div class="header"><h1>Chatbox</h1></div>
+        <div class="body">
+        <div class="wrapper">
+            <div class = "chatazinas">
+                おはようございます!
+            </div>
+        </div>
+        </div>
+        <div class="footer">
+            <form>                
+                <p>Name</p>
+                <input class="vards"/>
+                <p>Message</p>
+                <input class="myMsg"/>
+                <button type="submit" class="sutitZinu" onclick="sutitZinu()">SEND</button>
+            </form>
+        </div>
+    </div>
+
+        <!-- <div class="wrapper">
+
+            <div class = "chatazinas">
+                    おはようございます!
+            </div>
+
+            Vārds: <input class="vards" />
+            Ziņa: <input class="myMsg"/>
+            <button type="submit" class = "sutitZinu" onclick="sutitZinu()">Send</button>
+        
+        </div> -->
+    </body>
+    <script src="script.js"></script>
+</html>
