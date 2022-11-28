acfdfb9914ac7bb4e8d51094046cdf29647427e6
malinovskiss <90186408+malinovskiss@users.noreply.github.com>
2022-11-28 10:44:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-#script.js
-
 const API = 'http://127.0.0.1:81'
 
 let myMsg = document.querySelector('.myMsg')
@@ -11,10 +9,27 @@ function sutitZinu(){
     chatMsg.innerHTML += '<br/>' + myMsg.value
     fetch(API+'/send/'+vards.value+'/'+myMsg.value)
 }
-async function ieladetChataZinas(){
-    let datinoServera = await fetch(API = '/lasit')
-    let dati = await datiNoServera.json()
 
-    chataZinas
+async function loadChatMsg(){
+    let dataFromServer = await fetch(API + '/read')
+    let data = await dataFromServer.text()
+    
+    chatMsg.innerHTML = data
 }
 
+// setInterval(loadChatMsg,1499)
+
+async function loadChatMsgJson(){
+    let dataFromServer = await fetch(API + '/read')
+    let data = await dataFromServer.json()
+
+    chatMsg.innerHTML = ''
+
+    i=0;
+    while(i < await data.length){
+        chatMsg.innerHTML = chatMsg.innerHTML + '['+data[i]['laiks'] + '] '+ data[i]['vards'] + ': ' + data[i]['zina'] + '<br/>'
+        i+=1;
+    }
+} 
+
+setInterval(loadChatMsgJson,999)
